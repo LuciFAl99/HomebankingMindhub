ce86ee0680ad816a53200c438862e7c3afa50ecc
Lucila Alochis <lucila.alochis1999@gmail.com>
2023-05-26 20:48:47 -0300

    cambios en autorizaciones

--- src/main/java/com/mindhub/Homebanking/Configurations/WebAuthorization.java
+++ src/main/java/com/mindhub/Homebanking/Configurations/WebAuthorization.java
@@ -23,19 +23,19 @@ class WebAuthorization {
                .antMatchers(HttpMethod.POST,"/api/clients/current/pay-card").permitAll()
                .antMatchers(HttpMethod.POST, "/api/clients/**").permitAll()
                .antMatchers(HttpMethod.POST ,"/api/login", "/api/logout").permitAll()
-               .antMatchers( "/web/BigWing/**", "/web/Assets/**", "/web/login.html", "/web/registro.html").permitAll()
                .antMatchers("/manager.html").hasAuthority("ADMIN")
                .antMatchers("/api/admin/loan", "/web/Assets/Js/manager.html").hasAuthority("ADMIN")
                .antMatchers("/rest/**","/api/loans").hasAnyAuthority("ADMIN", "CLIENT")
                .antMatchers("/h2-console/**").hasAnyAuthority("ADMIN")
+               .antMatchers( "/web/BigWing/**","/web/login.html", "/web/registro.html", "/web/index.html","/web/Assets/**","/web/**").permitAll()
                .antMatchers("/api/accounts", "/api/clients",  "/api/logout").hasAuthority("ADMIN")
                .antMatchers(HttpMethod.POST, "/api/clients/current/export-pdf").hasAuthority("CLIENT")
                .antMatchers(HttpMethod.POST, "/api/clients/current/accounts/**", "/api/clients/current/cards/**", "/api/clients/current/transactions/**", "/api/clients/current/loans/**",  "/api/loans/**", "/api/current/loans/**").hasAnyAuthority("CLIENT", "ADMIN")
                .antMatchers(HttpMethod.PUT, "/api/clients/current/cards", "/api/clients/current/accounts/delete").hasAuthority("CLIENT")
                .antMatchers( "/api/clients/current/accounts/**", "/api/clients/current/cards/**", "/api/clients/current/transactions/**", "/api/clients/current", "/api/clients/current/loans/**", "/api/loans/**").hasAuthority("CLIENT")
                .antMatchers("/web/**").hasAuthority("CLIENT")
-               .antMatchers("/api/accounts/{id}").hasAnyAuthority("CLIENT", "ADMIN")
-               .anyRequest().denyAll();
+               .antMatchers("/api/accounts/{id}").hasAnyAuthority("CLIENT", "ADMIN");
+               //.anyRequest().denyAll();
 
         http.formLogin()
 
