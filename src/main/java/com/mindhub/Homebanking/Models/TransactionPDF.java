974e2150487f5fbd606e1016cd1b6463700fe71d
Lucila Alochis <lucila.alochis1999@gmail.com>
2023-05-29 22:47:59 -0300

    logo

--- src/main/java/com/mindhub/Homebanking/Models/TransactionPDF.java
+++ src/main/java/com/mindhub/Homebanking/Models/TransactionPDF.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class TransactionPDF {
     private Account account;
     private List<Transaction> listTransactions;
-    //private static final String LOGO_PATH = "E:\\Descargas\\DeployHomebanking\\src\\main\\resources\\static\\Web\\Assets\\Imágenes\\logo.png"; // Ruta del archivo de imagen del logo
+    private static final String LOGO_PATH = "static/Web/Assets/Imágenes/logo.png"; // Ruta del archivo de imagen del logo
 
     public TransactionPDF(List<Transaction> listTransactions, Account account) {
         this.listTransactions = listTransactions;
@@ -61,12 +61,12 @@ public class TransactionPDF {
         Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12);
         Font detailsFont = FontFactory.getFont(FontFactory.HELVETICA, 12);
 
-      /*  //Logo
+        //Logo
         Image logo = Image.getInstance(LOGO_PATH);
         logo.scaleToFit(200, 200);
-        logo.setAlignment(Image.ALIGN_LEFT);*/
+        logo.setAlignment(Image.ALIGN_LEFT);
 
-        //doc.add(logo);
+        doc.add(logo);
 
         // Título
         Paragraph title = new Paragraph("Lista de Transacciones", titleFont);
